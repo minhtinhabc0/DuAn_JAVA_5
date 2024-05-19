324756a9720c3b5a1a69ec8cba1292f9d3e6540f
Minh1009 <minhtinhabc0@gmail.com>
2024-05-19 13:29:51 +0700

    update

---
+++ src/main/java/com/example/demo/DuAnJava5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuAnJava5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuAnJava5Application.class, args);
+	}
+
+}
